177f9156e8c38cd6e4ec6d558745a06a483f28d9
三浦和也 <miurakazuya@Kazuya.local>
2019-09-23 11:12:35 +0900

    入室、退室処理の追加2

--- public/js/client.js
+++ public/js/client.js
@@ -2,11 +2,3 @@
 
 // オブジェクト
 const socket = io.connect();
-const name = prompt('名前を入力してください');
-joinUser(name);
-
-// 入室時の処理
-function joinUser(name) {
-    let roomId = 1;
-    socket.emit('join_user', { room: roomId, name: name});
-};
---
+++ public/js/room.js
@@ -0,0 +1,11 @@
+'use strict';  // 厳格モードとする
+
+// オブジェクト
+const name = prompt('名前を入力してください');
+joinUser(name);
+
+// 入室時の処理
+function joinUser(name) {
+    let roomId = 1;
+    socket.emit('join_user', { room: roomId, name: name});
+};
