4f069d580e407e25e0d2b0e52a3848b750eb7486
三浦和也 <miurakazuya@Kazuya.local>
2019-09-27 22:38:08 +0900

    得点処理の追加

--- libs/Battle.js
+++ libs/Battle.js
@@ -10,6 +10,9 @@ module.exports = class Battle {
         this.p1 = clients[0];
         this.p2 = clients[1];
         this.p3 = clients[2];
+        this.p1points = 0;
+        this.p2points = 0;
+        this.p3points = 0;
         this.turn = this.p1;
         this.drawer = this.p1;
         this.turn = 6;
@@ -30,6 +33,26 @@ module.exports = class Battle {
         };
     };
 
+    calcPoints(socket) {
+        if (socket.id === this.p1.id) {
+            this.p1points += 100;
+            if (this.drawer.id === this.p2.id) this.p2points += 100;
+            else this.p3points += 100;
+        }
+        else if (socket.id === this.p2.id) {
+            this.p2points += 100;
+            if (this.drawer.id === this.p1.id) this.p1points += 100;
+            else this.p3points += 100;
+        }
+        else {
+            this.p3points += 100;
+            if (this.drawer.id === this.p1.id) this.p1points += 100;
+            else this.p2points += 100;
+        }
+        // デバッグログ
+        console.log('p1points:'+this.p1points+' p2points:'+this.p2points+' p3points:'+this.p3points);
+    }
+
     start(io, socket) {
         if (socket.id === this.drawer.id) {
             io.to(this.room).emit('theme_to_drawer', {theme: this.gametheme[0], drawer: this.drawer});
@@ -37,8 +60,9 @@ module.exports = class Battle {
         }
 
         socket.on('msg_to_server', (msg) => {
-            if (msg === this.gametheme[0]) {
+            if (msg === this.gametheme[0] && socket.id !== this.drawer.id) {
                 console.log('正解！');
+                this.calcPoints(socket);
             }
         });
     }
